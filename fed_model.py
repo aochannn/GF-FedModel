6777e150e2850b92181eb87ff3b0a64160cd9abf
aochannn <aochan@brandeis.edu>
2023-08-15 16:32:46 +0800

    1

--- fed_model.py
+++ fed_model.py
@@ -1,6 +1,18 @@
 #Fed Model
 import matplotlib.pyplot as plt
 
+# earningPerShare = []
+# pricePerShare = []
+
+# def generateYield(a,b):
+#     ep = []
+#     for i in range(len(a)):
+#         ep.append(a[i]/b[i])
+#     sum=0
+#     for i in ep:
+#         sum += i
+#     return sum/len(ep)
+
 # Sample data
 x_data = [1, 2, 3, 4, 5]
 y_data1 = [2, 4, 6, 8, 10]
@@ -9,10 +21,7 @@ y_data2 = [1, 3, 5, 7, 9]
 # Create a new figure
 plt.figure()
 
-# Plot the first set of data
 plt.plot(x_data, y_data1, label='Dataset 1', color='blue', marker='o')
-
-# Plot the second set of data
 plt.plot(x_data, y_data2, label='Dataset 2', color='red', marker='x')
 
 # Add labels and title

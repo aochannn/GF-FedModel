b77d8429cb27e582358570d0ab07b1d7c275110c
aochannn <aochan@brandeis.edu>
2023-08-17 09:07:52 +0800

    1

--- fed_model.py
+++ fed_model.py
@@ -1,36 +1,39 @@
 #Fed Model
 import matplotlib.pyplot as plt
+import openpyxl
+path = "/Users/macbook/desktop/GF-FedModel/data.xlsx"
 
-# earningPerShare = []
-# pricePerShare = []
+x_data = []
+nominal_yield = []
+earnings_yield = []
 
-# def generateYield(a,b):
-#     ep = []
-#     for i in range(len(a)):
-#         ep.append(a[i]/b[i])
-#     sum=0
-#     for i in ep:
-#         sum += i
-#     return sum/len(ep)
+def readData(path):
+    wb = openpyxl.load_workbook(path)
+    sheet = wb.active
+    for i in range(11,sheet.max_row +1):
+        cell1 = sheet.cell(row = i, column = 2)
+        cell2 = sheet.cell(row = i, column = 3)
+        if (int(cell1.value)==0 or int(cell2.value)==0):
+            continue
+        else:
+            nominal_yield.append(float(cell1.value))
+            earnings_yield.append(1.0/(float(cell2.value)))
 
-# Sample data
-x_data = [1, 2, 3, 4, 5]
-y_data1 = [2, 4, 6, 8, 10]
-y_data2 = [1, 3, 5, 7, 9]
+readData(path)
+x_data = [i for i in range(1,len(earnings_yield)+1)]
 
-# Create a new figure
+
 plt.figure()
-
-plt.plot(x_data, y_data1, label='Dataset 1', color='blue', marker='o')
-plt.plot(x_data, y_data2, label='Dataset 2', color='red', marker='x')
-
-# Add labels and title
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.title('Two Sets of Data')
-
-# Add a legend
+plt.plot(x_data, nominal_yield, label='Treasury Bonds', color='blue', marker='o')
+plt.plot(x_data, earnings_yield, label='CSI 300', color='red', marker='x')
+plt.xlabel('Time(2013.8.15 - 2023.8.15)')
+plt.ylabel('Rate %')
+plt.title('Fed Model')
 plt.legend()
-
-# Show the plot
 plt.show()
+
+
+
+
+
+
